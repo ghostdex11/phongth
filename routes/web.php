961a9a78e2d08cc1e5825d203b2a5074e6a9f80a
chien <chiendang588@gmail.com>
2021-06-01 16:23:36 +0700

    router fix

--- routes/web.php
+++ routes/web.php
@@ -59,5 +59,5 @@ Route::group(['prefix' => 'admin', 'middleware' => 'adminCheck'], function () {
 });
 
 
-Route::get('page','App\Http\Controllers\pageController@page');
+Route::get('home','App\Http\Controllers\pageController@page');
 
